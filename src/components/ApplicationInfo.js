74002f9cc1001fd51177eea5b73a8db7aa012fe6
bahasaracoglu <bahasaracoglu@gmail.com>
2024-01-01 21:59:14 +0300

    applicationInfo & css

--- src/components/ApplicationInfo.js
+++ src/components/ApplicationInfo.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
+import { Timestamp, doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useParams } from "react-router-dom";
+import Loader from "./Loader";
 
 function ApplicationInfo() {
   const applicationId = useParams().basvuruNo;
   console.log(applicationId);
   const [applicationData, setApplicationData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [createdDate, setCreatedDate] = useState(null); // Değişiklik burada
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +21,9 @@ function ApplicationInfo() {
         if (docSnap.exists()) {
           console.log("Document data:", docSnap.data());
           setApplicationData(docSnap.data());
+          const dateObj = docSnap.data().createdAt.toDate();
+          setCreatedDate(dateObj.toLocaleDateString("tr-TR"));
+          setLoading(false);
         } else {
           console.log("No such document!");
         }
@@ -30,26 +36,79 @@ function ApplicationInfo() {
   }, [applicationId]); // useEffect'i sadece applicationId değiştiğinde çalıştır
 
   return (
-    <section className="min-h-screen flex  justify-center items-center   ">
-      <div className="flex flex-col my-6 w-5/6 p-8 gap-4 rounded-xl bg-white shadow-xl lg:w-3/6 ">
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col my-6 w-5/6 p-8 gap-4 rounded-xl max-w-screen-md bg-white shadow-xl">
         <h1 className="pt-8 pb-8 font-bold text-xl">Başvuru Bilgileri</h1>
-        {applicationData && (
-          <ul className=" bg-slate-200 p-2 rounded-xl flex flex-col">
-            <li>Ad: {applicationData.firstName}</li>
-            <li>Soyad: {applicationData.lastName}</li>
-            <li>Yaş: {applicationData.age}</li>
-            <li>TC Kimlik No: {applicationData.tcNo}</li>
-            <li>Başvuru Nedeni: {applicationData.purposeOfApp}</li>
-            <li>Adres: {applicationData.address}</li>
-            <li>
-              Ek Dosya:
-              <a
-                href={applicationData.additionalFile}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Dosyayı Aç
-              </a>
+        {loading ? (
+          <Loader />
+        ) : (
+          <ul className="bg-slate-200 p-4 rounded-xl flex flex-col gap-4">
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Ad:</span>
+              <span className="ml-auto text-left w-1/2 flex flex-wrap">
+                {applicationData.firstName}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Soyad:</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.lastName}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">T.C. Kimlik Numarası</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.tcNo}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Yaş:</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.age}
+              </span>
+            </li>{" "}
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Adres</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.address}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Başvuru Sebebi:</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.purposeOfApp}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Başvuru Tarihi:</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {createdDate}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Başvuru Durumu:</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.status}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Başvuru Yanıtı</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                {applicationData.response || "Henüz yanıtlanmadı"}
+              </span>
+            </li>
+            <li className="flex justify-between items-center border-b border-indigo-200 pb-2">
+              <span className="font-medium w-1/2">Ek Dosya</span>
+              <span className="ml-auto text-left w-1/2 overflow-hidden overflow-ellipsis">
+                <a
+                  href={applicationData.additionalFile}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline font-medium"
+                >
+                  Dosyayı Aç
+                </a>
+              </span>
             </li>
           </ul>
         )}
--- src/components/ApplicationInquiry.js
+++ src/components/ApplicationInquiry.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const schema = yup
   .object({
@@ -11,28 +13,57 @@ const schema = yup
   .required();
 
 function ApplicationInquiry() {
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
+  useEffect(() => {
+    setIsInvalid(false);
+  }, [watch("applicationNo")]);
   const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    navigate(`/basvuru/${data.applicationNo}`);
+  const checkApplication = async (data) => {
+    try {
+      setLoading(true);
+      const docRef = doc(db, "applications", data.applicationNo);
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        navigate(`/basvuru/${data.applicationNo}`);
+      } else {
+        console.log("No such document!");
+        setIsInvalid(true);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+
     console.log(data);
   };
+
+  const onSubmit = (data) => {
+    checkApplication(data);
+  };
+
   return (
-    <section className="min-h-screen flex  justify-center items-center   ">
+    <section className="min-h-screen flex justify-center items-center">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col my-6 w-5/6 p-8 gap-4 rounded-xl bg-white shadow-xl lg:w-3/6 "
+        className="flex flex-col my-6 w-5/6 p-8 gap-4 rounded-xl max-w-screen-md bg-white shadow-xl"
       >
         <h1 className="pt-8 pb-8 font-bold text-xl">Başvuru Sorgula</h1>
         <div className="flex flex-col gap-4  ">
-          <label className="flex justify-between font-bold gap-2">
+          <label className="flex flex-col justify-between font-bold gap-2">
             Başvuru No:
             <input
               defaultValue="123456"
@@ -41,13 +72,24 @@ function ApplicationInquiry() {
             />
           </label>
         </div>
-
         {errors.exampleRequired && <span>This field is required</span>}
-
-        <input
-          className="p-2 text-white bg-sky-600 font-bold rounded-xl"
-          type="submit"
-        />
+        <div className="flex flex-col gap-2 h-24 justify-center">
+          {/* Yüklenme durumuna göre button rengini ve içeriğini değiştir */}
+          <button
+            className={`p-2 text-white font-bold rounded-xl ${
+              loading ? "bg-gray-500" : "bg-sky-600"
+            }`}
+            type="submit"
+            disabled={loading} // Yüklenme durumundayken butonu devre dışı bırak
+          >
+            {loading ? "Yükleniyor..." : "Gönder"}
+          </button>
+          {isInvalid && (
+            <div className=" bg-rose-100 border-red-400 border p-2 text-center ">
+              Hatalı başvuru numarası!
+            </div>
+          )}
+        </div>
       </form>
     </section>
   );
